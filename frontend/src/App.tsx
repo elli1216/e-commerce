8634c047d40fa10659b5b4e2ff96da35abb6fd70
Romeo <romeoquinones4@gmail.com>
2025-05-10 13:37:20 +0800

    feat: implement routing in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
 import Signup from './pages/Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = (): React.JSX.Element => {
-  return <Signup />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
