28b5c15b9940aa4369f9a99b5b814686ad23010f
Romeo <romeoquinones4@gmail.com>
2025-05-10 19:37:24 +0800

    feat: implement user signup api and store the new user in xml

---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,8 @@
+import express, { Router } from 'express';
+import { signup } from '../controllers/auth.controller';
+
+const router: Router = express.Router();
+
+router.post('/signup', signup);
+
+export default router;
