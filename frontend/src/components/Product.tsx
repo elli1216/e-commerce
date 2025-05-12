35dcce1ec1e96847bad45ba5b699a9ff812c7ab4
Romeo <romeoquinones4@gmail.com>
2025-05-12 21:57:09 +0800

    refactor(Product): added hover style and Select component

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
+import Select from './Select';
 
 const Product = (): React.JSX.Element => {
   return (
-    <div className="flex flex-col w-fit gap-2">
+    <div className="flex flex-col w-fit hover:bg-base-300 transition-all">
       <div>
         <img src="https://picsum.photos/200" alt="Product image" />
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 p-2">
         <span>Product name</span>
         <span>Price</span>
-        <select name="quantity" className="select select-sm w-fit">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
+        <Select />
+        <button className='btn btn-sm btn-secondary self-center'>
+          Add to cart
+        </button>
       </div>
-      <button className='btn btn-sm btn-secondary self-center'>
-        Add to cart
-      </button>
     </div>
   );
 };
