5e8d6b2fcfcf63098d9c756067371b944fe6ee70
Romeo <romeoquinones4@gmail.com>
2025-05-19 09:35:33 +0800

    feat(Product): showed badge when added cart

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -10,6 +10,8 @@ interface ProductProps {
 const Product = ({ product, onAddToCart }: ProductProps): React.JSX.Element => {
 
   const [quantity, setQuantity] = React.useState<number>(1);
+  const [showAdded, setShowAdded] = React.useState<boolean>(false);
+  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
 
   const handleQuantityChange = (
     e: React.ChangeEvent<HTMLSelectElement>
@@ -17,6 +19,21 @@ const Product = ({ product, onAddToCart }: ProductProps): React.JSX.Element => {
     setQuantity(Number(e.target.value));
     console.log(quantity)
   }
+
+  const handleAddToCart = (): void => {
+    onAddToCart(product, quantity);
+    setShowAdded(true);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => setShowAdded(false), 2000);
+  }
+
+  React.useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col w-fit hover:bg-base-300 transition-all">
@@ -26,18 +43,29 @@ const Product = ({ product, onAddToCart }: ProductProps): React.JSX.Element => {
       <div className="flex flex-col gap-2 p-2">
         <span>{product.productName}</span>
         <span>₱{product.productPrice}</span>
-        <Select
-          onQuantityChange={handleQuantityChange}
-          quantity={quantity}
-        />
+        <div className="flex flex-row justify-between">
+          <Select
+            onQuantityChange={handleQuantityChange}
+            quantity={quantity}
+          />
+          <div className={`badge badge-success bg-accent ${showAdded ? 'opacity-100' : 'opacity-0'} transition-opacity`}>
+            <svg className="size-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
+                <circle cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2" />
+                <polyline points="7 13 10 16 17 8" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2" />
+              </g>
+            </svg>
+            Addded
+          </div>
+        </div>
         <button
-          onClick={() => onAddToCart(product, quantity)}
+          onClick={handleAddToCart}
           className='btn btn-sm btn-secondary self-center'
         >
           Add to cart
         </button>
       </div>
-    </div>
+    </div >
   );
 };
 
